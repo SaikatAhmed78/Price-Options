259e9fe1e59000e2aa4209cffa9a5fae5138dffc
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-10-28 19:06:25 +0600

    Responsive Navbar using react and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import BasicNav from './Components/Navbar/BasicNav/BasicNav'
+// import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
       
+      {/* <Navbar></Navbar> */}
+      <BasicNav></BasicNav>
       <h1>Vite + React</h1>
       
     </>
